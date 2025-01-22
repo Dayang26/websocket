fb93654496e12ffc1be485a654505238f4853ca8
Aaron <flyhsyy@gmail.com>
2025-01-22 18:40:21 +0700

    continue..........

--- server.go
+++ server.go
@@ -2,6 +2,8 @@ package websocket
 
 import (
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 )
 
@@ -28,12 +30,111 @@ type Upgrader struct {
 	EnableCompression bool
 }
 
-//// Upgrade http协议升级到WebSocket
-//func (u *Upgrader) Upgrade(w http.ResponseWriter, r http.Request, responseHeader http.Header) (*Conn, error) {
-//	const badHandshake = "websocket: the client is not using the websocket protocol"
-//	if !tokenListContainsValues {
-//
-//	}
-//
-//	return nil, nil
-//}
+type HandshakeError struct {
+	message string
+}
+
+func (e HandshakeError) Error() string {
+	return e.message
+}
+
+// Upgrade http协议升级到WebSocket
+func (u *Upgrader) Upgrade(w http.ResponseWriter, r *http.Request, responseHeader http.Header) (*Conn, error) {
+	const badHandshake = "websocket: the client is not using the websocket protocol"
+
+	if !tokenListContainsValues(r.Header, "Connection", "upgrade") {
+		return u.returnError(w, r, http.StatusBadRequest, badHandshake+" ,'upgrade': token not found in 'Connection' header")
+	}
+
+	if !tokenListContainsValues(r.Header, "Upgrade", "websocket") {
+		w.Header().Set("Upgrade", "websocket")
+		return u.returnError(w, r, http.StatusBadRequest, badHandshake+" ,'websocket': token not found in 'Upgrade' header")
+	}
+
+	if r.Method != http.MethodGet {
+		return u.returnError(w, r, http.StatusMethodNotAllowed, badHandshake+" "+r.Method+" not allowed")
+	}
+
+	if !tokenListContainsValues(r.Header, "Sec-WebSocket-Version", "13") {
+		w.Header().Set("Upgrade", "websocket")
+		return u.returnError(w, r, http.StatusBadRequest, badHandshake+" ,'websocket': unsupported version :13 not found in 'Sec-WebSocket-Version' header")
+	}
+
+	// 握手阶段,响应头
+	if _, ok := responseHeader["Sec-WebSocket-Extensions"]; !ok {
+		return u.returnError(w, r, http.StatusInternalServerError, badHandshake+" websocket: application specific 'Sec-WebSocket-Extensions' header are not unsupported")
+	}
+
+	checkOrigin := u.CheckOrigin
+	if checkOrigin == nil {
+		checkOrigin = checkSameOrigin
+	}
+
+	if !checkOrigin(r) {
+		return u.returnError(w, r, http.StatusForbidden, "websocket: request origin not allowed by Upgrader.CheckOrigin")
+	}
+
+	challengeKey := r.Header.Get("Sec-WebSocket-Key")
+	if !isValidChallengeKey(challengeKey) {
+		return u.returnError(w, r, http.StatusBadRequest, "websocket: not a websocket handshake :'Sec-WebSocket-Key' header is invalid")
+	}
+
+	subprotocol := u.selectSubprotocol(r, responseHeader)
+
+	return nil, nil
+}
+
+func (u *Upgrader) returnError(w http.ResponseWriter, r *http.Request, status int, reason string) (*Conn, error) {
+	err := &HandshakeError{reason}
+	if u.Error != nil {
+		u.Error(w, r, status, err)
+	} else {
+		w.Header().Set("Sec-WebSocket-Version", "13")
+		http.Error(w, http.StatusText(status), status)
+	}
+
+	return nil, err
+}
+
+func checkSameOrigin(r *http.Request) bool {
+	origin := r.Header["Origin"]
+	if len(origin) == 0 {
+		return true
+	}
+
+	u, err := url.Parse(origin[0])
+	if err != nil {
+		return false
+	}
+
+	return equalASCIIFold(u.Host, r.Host)
+}
+
+func Subprotocols(r *http.Request) []string {
+	h := strings.TrimSpace(r.Header.Get("Sec-WebSocket-Protocol"))
+	if h == "" {
+		return nil
+	}
+
+	procotols := strings.Split(h, ",")
+	for i := range procotols {
+		procotols[i] = strings.TrimSpace(procotols[i])
+	}
+	return procotols
+}
+func (u *Upgrader) selectSubprotocol(r *http.Request, responseHeader http.Header) string {
+	if u.SubProtocols != nil {
+		clientProtocols := Subprotocols(r)
+		for _, clientProcotol := range clientProtocols {
+			for _, serverProtocol := range u.SubProtocols {
+				if clientProcotol == serverProtocol {
+					return clientProcotol
+				}
+			}
+		}
+	} else if u.SubProtocols == nil {
+		return responseHeader.Get("Sec-WebSocket-Protocol")
+	}
+
+	return ""
+}
--- util.go
+++ util.go
@@ -1,5 +1,11 @@
 package websocket
 
+import (
+	"encoding/base64"
+	"net/http"
+	"unicode/utf8"
+)
+
 func skipSpace(s string) (rest string) {
 	i := 0
 	for ; i < len(s); i++ {
@@ -10,21 +16,158 @@ func skipSpace(s string) (rest string) {
 	return s[i:]
 }
 
-//func nextToken(s string) (token, rest string) {
-//	i := 0
-//	for ; i < len(s); i++ {
-//		if !isTokenObject[s[i]] {
-//			break
-//		}
-//	}
-//	return s[:i], s[i:]
-//}
-//
-//func tokenListContainsValues(header http.Header, name, value string) bool {
-//	for _, s := range header[name] {
-//		for {
-//			var t string
-//			t, s = nextToken(skipSpace(s))
-//		}
-//	}
-//}
+// Token octets per RFC 2616.
+var isTokenOctet = [256]bool{
+	'!':  true,
+	'#':  true,
+	'$':  true,
+	'%':  true,
+	'&':  true,
+	'\'': true,
+	'*':  true,
+	'+':  true,
+	'-':  true,
+	'.':  true,
+	'0':  true,
+	'1':  true,
+	'2':  true,
+	'3':  true,
+	'4':  true,
+	'5':  true,
+	'6':  true,
+	'7':  true,
+	'8':  true,
+	'9':  true,
+	'A':  true,
+	'B':  true,
+	'C':  true,
+	'D':  true,
+	'E':  true,
+	'F':  true,
+	'G':  true,
+	'H':  true,
+	'I':  true,
+	'J':  true,
+	'K':  true,
+	'L':  true,
+	'M':  true,
+	'N':  true,
+	'O':  true,
+	'P':  true,
+	'Q':  true,
+	'R':  true,
+	'S':  true,
+	'T':  true,
+	'U':  true,
+	'W':  true,
+	'V':  true,
+	'X':  true,
+	'Y':  true,
+	'Z':  true,
+	'^':  true,
+	'_':  true,
+	'`':  true,
+	'a':  true,
+	'b':  true,
+	'c':  true,
+	'd':  true,
+	'e':  true,
+	'f':  true,
+	'g':  true,
+	'h':  true,
+	'i':  true,
+	'j':  true,
+	'k':  true,
+	'l':  true,
+	'm':  true,
+	'n':  true,
+	'o':  true,
+	'p':  true,
+	'q':  true,
+	'r':  true,
+	's':  true,
+	't':  true,
+	'u':  true,
+	'v':  true,
+	'w':  true,
+	'x':  true,
+	'y':  true,
+	'z':  true,
+	'|':  true,
+	'~':  true,
+}
+
+func nextToken(s string) (token, rest string) {
+	i := 0
+	for ; i < len(s); i++ {
+		if !isTokenOctet[s[i]] {
+			break
+		}
+	}
+	return s[:i], s[i:]
+}
+
+// equalASCIIFold returns true if s is equal to t with ASCII case folding as
+// defined in RFC 4790.
+func equalASCIIFold(s, t string) bool {
+	for s != "" && t != "" {
+		sr, size := utf8.DecodeRuneInString(s)
+		s = s[size:]
+		tr, size := utf8.DecodeRuneInString(t)
+		t = t[size:]
+		if sr == tr {
+			continue
+		}
+		if 'A' <= sr && sr <= 'Z' {
+			sr = sr + 'a' - 'A'
+		}
+		if 'A' <= tr && tr <= 'Z' {
+			tr = tr + 'a' - 'A'
+		}
+		if sr != tr {
+			return false
+		}
+	}
+	return s == t
+}
+
+// tokenListContainsValues 解析协议
+// 本质是处理header中的参数.header = map[string][]string
+// 每个String切片可能是s = ["token1","token2"]
+func tokenListContainsValues(header http.Header, name, value string) bool {
+
+headers:
+	for _, s := range header[name] {
+		for {
+			var t string
+			t, s = nextToken(skipSpace(s))
+			if t == "" {
+				continue headers
+			}
+
+			s = skipSpace(s)
+			if s != "" && s[0] != ',' {
+				continue headers
+			}
+
+			if equalASCIIFold(t, value) {
+				return true
+			}
+
+			if s == "" {
+				continue headers
+			}
+			s = s[1:]
+		}
+	}
+	return false
+}
+
+func isValidChallengeKey(s string) bool {
+	if s == "" {
+		return false
+	}
+
+	decoded, err := base64.StdEncoding.DecodeString(s)
+	return err == nil && len(decoded) == 16
+}
